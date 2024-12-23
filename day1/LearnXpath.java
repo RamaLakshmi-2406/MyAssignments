db75b73ffd284055c03b3fbe02a969a29a5c375f
RamaLakshmi-2406 <ramalakshmir952@gmail.com>
2024-12-23 19:22:23 +0530

    Add files via upload
    week2.day1

---
+++ day1/LearnXpath.java
@@ -0,0 +1,21 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnXpath {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("testleaf.2023@gmail.com");
+		driver.findElement(By.xpath("//input[contains(@name,'pas')]")).sendKeys("Tuna@321");
+		driver.findElement(By.xpath("//button[@name='login']")).click();
+		Thread.sleep(5000);
+		
+;
+	}
+
+}
